95b877e8f05678c75da225a9b7f8d214cb7d23a3
uchagani <umair.chagani@gmail.com>
2018-11-12 21:38:07 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='prayer_times_calculator',
+      version='0.0.1',
+      description='Prayer Times Calculator',
+      url='https://github.com/uchagani/prayer_times_calculator',
+      author='Umair Chagani',
+      author_email='umair.chagani@gmail.com',
+      license='MIT',
+      packages=['prayer_times_calculator'],
+      zip_safe = False)
