ab61e5c766430060ac6f346876db1382285c5669
Umair Chagani <umair.chagani@gmail.com>
2022-11-01 21:18:55 -0400

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='prayer_times_calculator',
-      version='0.0.6',
+      version='0.0.7',
       description='Prayer Times Calculator',
       url='https://github.com/uchagani/prayer-times-calculator',
       author='Umair Chagani',
