f86a0052be136c4c697ca34de50917af85f88aca
Yazan Majadba <yazanmy1998@hotmail.com>
2024-03-17 23:50:03 +0300

    Add new calculation methods
    Add new calculation methods
    
    https://api.aladhan.com/v1/methods

--- prayer_times_calculator/pray_times_calculator.py
+++ prayer_times_calculator/pray_times_calculator.py
@@ -26,6 +26,14 @@ CALCULATION_METHODS: Final = {
     "turkey": 13,
     "russia": 14,
     "moonsighting": 15,
+    "dubai": 16,
+    "jakim": 17,
+    "tunisia": 18,
+    "algeria": 19,
+    "kemenag": 20,
+    "morocco": 21,
+    "portugal": 22,
+    "jordan": 23,
     "custom": 99,
 }
 
